4f7c912dc34a82a2acae2eada897d64cdd46cda0
Samar Haytamy <ssa10@fayoum.edu.eg>
2023-09-28 22:06:50 -0400

    Create mymodule.py

---
+++ unittest-example/mymodule.py
@@ -0,0 +1,16 @@
+import unittest
+from mymodule import square, double
+
+class TestSquare(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(square(2), 4)
+        self.assertEqual(square(3.0), 9.0)
+        self.assertEqual(square(-3), 9)
+
+class TestDouble(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(double(2), 4)
+        self.assertEqual(double(-3.1), -6.2)
+        self.assertEqual(double(0), 0)
+
+unittest.main()
