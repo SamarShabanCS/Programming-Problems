5caad59282a886c6e617b72e13bca2de7476a857
Samar Haytamy <ssa10@fayoum.edu.eg>
2023-09-28 22:07:48 -0400

    Update mymodule.py

--- unittest-example/mymodule.py
+++ unittest-example/mymodule.py
@@ -1,16 +1,5 @@
-import unittest
-from mymodule import square, double
+def square(number):
+    return number ** 2
 
-class TestSquare(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(square(2), 4)
-        self.assertEqual(square(3.0), 9.0)
-        self.assertEqual(square(-3), 9)
-
-class TestDouble(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(double(2), 4)
-        self.assertEqual(double(-3.1), -6.2)
-        self.assertEqual(double(0), 0)
-
-unittest.main()
+def double(number):
+    return number * 2
